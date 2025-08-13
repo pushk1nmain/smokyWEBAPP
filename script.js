1b1282ff71caa3ea1f82b9da6190b1fe61d5e727
pushk1nmain <pushk1nmain@gmail.com>
2025-08-13 19:22:41 +0300

    bag fix

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ const getUserData = () => {
 const personalizeGreeting = (user) => {
     const titleElement = document.querySelector('.main-title');
     if (titleElement && user.first_name) {
-        titleElement.textContent = `Привет, ${user.first_name}! Я Смоки — ваш робот-друг на пути к жизни без сигарет!`;
+        titleElement.textContent = `Привет, ${user.first_name}! Я Смоки — ваш робот-друг на пути к жизни без сигарет`;
     }
 };
 
